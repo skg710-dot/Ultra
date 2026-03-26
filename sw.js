862eeeeb1c5270564489e370d7dc79c59dcf91fc
skg710-dot <skg710@gmail.com>
2026-03-27 00:17:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kartavya-ultra-final-v4';
+const CACHE_NAME='kartavya-ultra-final-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
